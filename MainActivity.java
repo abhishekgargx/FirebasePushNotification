86badd6c3426a44997eddd501d47c1072354c770
Abhishek Garg <abhishekgarg727@gmail.com>
2019-01-28 16:23:08 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,9 @@
+package com.name.abhishek
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
