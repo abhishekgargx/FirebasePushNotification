f8855b52c6235451f813fb5feda6bdea8996bca2
Abhishek Garg <abhishekgarg727@gmail.com>
2019-12-05 18:18:56 +0530

    Update SplashActivity.java

--- SplashActivity.java
+++ SplashActivity.java
@@ -1,40 +1,50 @@
 public class SplashActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-       
-        // when app is in background or swiped from recent , handle custom data or intents or actions like below
-        // below code should always in your app launcher activity , onCreate method , in my case it is SplashActivity 
-        if (getIntent().getExtras() != null) {
-             Bundle bundle = getIntent().getExtras();
-                    if(bundle != null) { 
-                        // activity_name is key name of custom data i sending
-                       String  value = bundle.getString("activity_name");
-                         if( value != null){
-                             // perform any condition you want to execute based on value
-                             // i am starting google.com in web browser
-                          switch(value){
-                                case "google":{
-                                  startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.google.com")));
-                                  break;
-                                 }
-                                 default:{
-                                    startActivity(new Intent(this, MainActivity.class) );
-                                    break;
-                               	}
-                              }
-                        }else{
-                           startActivity(new Intent(this, MainActivity.class) );
-                         }
-                    }
-                    else{
-                       startActivity(new Intent(this, MainActivity.class) );
-                   }
+        if (getIntent().getExtras() != null
+                && getIntent().getExtras().getString(ClassConstants.Notification.ACTIVITY_NAME) != null
+                && SharedPrefManager.getInstance(this).get(SharedPrefManager.Key.IS_SIGN_IN_COMPLETED,
+                false)) {
+            // access bundle data
+            Bundle bundle = getIntent().getExtras();
+            String activity_name = bundle.getString(ClassConstants.Notification.ACTIVITY_NAME);
+            String chatRoomId = bundle.getString(ClassConstants.Notification.CHAT_ROOM);
+            String chatName = bundle.getString(ClassConstants.Notification.CHAT_NAME);
+            String chatType = bundle.getString(ClassConstants.Notification.CHAT_TYPE);
+            // resolve activity
+            Intent intent = ClassUtility.createIntentUsingClassName(this, activity_name);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            // handle chat intent
+            if (TextUtils.equals(activity_name, ClassConstants.Notification.CHAT_ACTIVITY)) {
+                intent.putExtra(ClassConstants.Bundle.ID, chatRoomId);
+                intent.putExtra(ClassConstants.Bundle.NAME, chatName);
+                intent.putExtra(ClassConstants.Bundle.TYPE, chatType);
+            }
+            // task builder
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            stackBuilder.addNextIntentWithParentStack(intent);
+            stackBuilder.startActivities();
+            finish();
+        } else {
+            launchNextScreen();
         }
-        else{
-           startActivity(new Intent(this, MainActivity.class) );
-       }
-      
     }
-}
+
+    private void launchNextScreen() {
+        Intent intent;
+        // welcome screen shown or not
+        if (!SharedPrefManager.getInstance(this).get(SharedPrefManager.Key.IS_BOARDING_COMPLETED, false)) {
+            intent = new Intent(SplashActivity.this, WelcomeScreenActivity.class);
+        }
+        // if sign not completed or access token is null
+        else if (!SharedPrefManager.getInstance(this).get(SharedPrefManager.Key.IS_SIGN_IN_COMPLETED, false) ||
+                SharedPrefManager.getInstance(this).get(SharedPrefManager.Key.ACCESS_TOKEN, null) == null) {
+            intent = new Intent(SplashActivity.this, SignInActivity.class);
+        } else {
+            intent = new Intent(SplashActivity.this, DashboardActivity.class);
+        }
+        startActivity(intent);
+        finish();
+    }
